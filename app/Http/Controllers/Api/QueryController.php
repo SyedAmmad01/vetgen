ed8b2c41aec6b91347dddb064d4d5746c1fbb1a0
Syed Ammad Hussain Shah <lapzone.pk@Apples-MacBook-Pro.local>
2026-01-31 18:54:51 +0500

    changes

--- app/Http/Controllers/Api/QueryController.php
+++ app/Http/Controllers/Api/QueryController.php
@@ -10,6 +10,7 @@ class QueryController extends Controller
 {
     public function query_store(Request $request)
     {
+        // dd($request->all());
         $var = $request->all();
         $z = json_encode($var);
 
@@ -26,7 +27,8 @@ class QueryController extends Controller
             'city'          => $fields['field_f27e238']['value'] ?? null,
             'area'          => $fields['field_ce9e56a']['value'] ?? null,
             'property_type' => $fields['field_0713424']['value'] ?? null,
-            'attend'        => null, // because no field in JSON
+            'attend'        => 1, // because no field in JSON
+            'promo_code'    => $fields['field_4e60009']['value'] ?? null,
             'dump'          => $z,
         ]);
 
